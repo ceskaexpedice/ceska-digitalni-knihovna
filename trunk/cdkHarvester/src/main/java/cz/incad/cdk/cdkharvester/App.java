1b8a7e9fd905bd622c685f7713cef3ff4fba500b
alberto.en.vlasim@gmail.com <alberto.en.vlasim@gmail.com@f195b8b0-3c76-b1d5-f26c-81c947884e86>
2013-10-07 12:43:33 +0000

    cdk

--- trunk/cdkHarvester/src/main/java/cz/incad/cdk/cdkharvester/App.java
+++ trunk/cdkHarvester/src/main/java/cz/incad/cdk/cdkharvester/App.java
@@ -6,7 +6,8 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         CDKImportProcess p = new CDKImportProcess();
-        p.start("http://vmkramerius.incad.cz:8080/search", "vmkramerius", "krameriusAdmin", "krameriusAdmin");
+        p.start("kk", "vmkramerius", "krameriusAdmin", "krameriusAdmin");
+        //p.start("http://vmkramerius.incad.cz:8080/search", "vmkramerius", "krameriusAdmin", "krameriusAdmin");
         //p.start("http://localhost:8080/search", "alberto", "krameriusAdmin", "krameriusAdmin");
     }
 }
