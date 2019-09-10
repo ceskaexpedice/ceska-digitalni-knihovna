6faa1df67d6921595c687a5aa8733c16a59efff7
pavel.stastny <pavel.stastny@accenture.com>
2019-09-10 21:29:28 +0200

    Getting rid off lemmatized fields, new version

--- tools/cdkHarvester/src/main/java/cz/incad/cdk/cdkharvester/process/solr/RemoveLemmatizedFields.java
+++ tools/cdkHarvester/src/main/java/cz/incad/cdk/cdkharvester/process/solr/RemoveLemmatizedFields.java
@@ -4,6 +4,7 @@ import cz.incad.kramerius.utils.XMLUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.xml.transform.TransformerException;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.List;
@@ -14,20 +15,25 @@ public class RemoveLemmatizedFields implements ProcessSOLRXML {
     public byte[] process(String url, String pid, InputStream is) throws Exception {
         Document document = XMLUtils.parseDocument(is, false);
         Element doc = XMLUtils.findElement(XMLUtils.findElement(document.getDocumentElement(), "result"), "doc");
-
         List<Element> elements1 = XMLUtils.getElements(doc, new XMLUtils.ElementsFilter() {
             @Override
             public boolean acceptElement(Element element) {
                 String name = element.getAttribute("name");
-                if (name.contains("_lemmatized_")) {
+                if (name.contains("_lemmatized")) {
                     return true;
-                }
-                return false;
+                } else return false;
             }
         });
 
         for (Element toRemove :  elements1) {  toRemove.getParentNode().removeChild(toRemove); }
 
+        document.normalize();
+
+        byte[] barr2 = renderXML(document);
+        return barr2;
+    }
+
+    private byte[] renderXML(Document document) throws TransformerException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         XMLUtils.print(document, bos);
         return bos.toByteArray();
